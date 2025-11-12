0c86b7102223dad8e86ff0209055ef134a59120c
Leonardo Previato <leonardopreviato0309@gmail.com>
2025-11-11 21:19:00 -0300

    Segundo exercício de python

---
+++ Mundo 1/Respondendo ao usuário ex2.py
@@ -0,0 +1,4 @@
+# Solicita o nome do usuário e armazena na variavel 'Nome_usuario'
+nome_usuario = input("Digite seu nome: ")
+# Exibe uma mensagem de boas-vindas perosnalizada 
+print(f"Olá {nome_usuario}! É um prazer te conhecer!")
