dc005b1ff73eecc2c09815ed7049397ab0af7fd5
Leonardo Previato <leonardopreviato0309@gmail.com>
2025-11-11 21:25:49 -0300

    Somando dois números

---
+++ Mundo 1/Somando dois números ex3.py
@@ -0,0 +1,11 @@
+# Programa para somar dois números
+# Este programa solicita que o usuário digite dois números e exibe a soma deles
+
+# Coleta o primeiro número do usuário e converte para inteiro
+num1 = int(input("Digite o primeiro número: "))
+
+# Coleta o segundo número do usuário e converte para inteiro
+num2 = int(input("Digite o segundo número: "))
+
+# Exibe o resultado da soma usando f-string para formatar a mensagem
+print(f"A soma entre {num1} + {num2} é igual a {num1 + num2}")
