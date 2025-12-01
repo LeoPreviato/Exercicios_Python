0c6f0fa363a96ac8b3146bedbc508e1920be7792
Leonardo Previato <leonardopreviato0309@gmail.com>
2025-12-01 20:18:37 -0300

    Squashed commit of the following:
    commit 8f9e9eca66a9443a9b391869b2d54cedf4df4b6d
    Author: Leonardo Previato <leonardopreviato0309@gmail.com>
    Date:   Mon Dec 1 20:18:24 2025 -0300
    
        atualização no Tocando um Mp3
    
        Adicionei comentarios

--- Mundo 1/Tocando um Mp3 ex21.py
+++ Mundo 1/Tocando um Mp3 ex21.py
@@ -1,12 +1,19 @@
+# Importa o módulo pygame, que é uma biblioteca para desenvolvimento de jogos e multimídia em Python
 import pygame
 
+# Inicializa o mixer de áudio do pygame, necessário para reproduzir sons e músicas
 pygame.mixer.init()
 
+# Carrega o arquivo de música especificado (neste caso, "Tocando um mp3.mp3") para o mixer
 pygame.mixer.music.load("Tocando um mp3.mp3")
 
+# Define o volume da música para 70% (0.7 é um valor entre 0.0 e 1.0)
 pygame.mixer.music.set_volume(0.7)
 
+# Inicia a reprodução da música carregada
 pygame.mixer.music.play()
 
+# Entra em um loop que continua enquanto a música estiver sendo reproduzida (get_busy() retorna True se estiver tocando)
 while pygame.mixer.music.get_busy():
+    # O 'pass' é uma instrução vazia; o loop aguarda até que a música termine
     pass
